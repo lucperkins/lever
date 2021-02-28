d84efbc2f9bf690b845eca608fb15cf894404c74
Luc Perkins <luc@timber.io>
2021-02-27 18:02:13 -0800

    Add initial structopt integration

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use async_graphql_warp::{graphql_subscription, Response};
 use lever::error::Error;
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use structopt::StructOpt;
 use warp::{http::Response as HttpResponse, Filter};
 
 type GraphQLSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
@@ -28,21 +29,22 @@ impl Server {
     }
 
     async fn run(&self) -> Result<(), Error> {
-        let graphql_post = async_graphql_warp::graphql(self.schema.clone()).and_then(
-            |(schema, request): (
-                Schema<QueryRoot, EmptyMutation, EmptySubscription>,
-                async_graphql::Request,
-            )| async move {
-                Ok::<_, Infallible>(Response::from(schema.execute(request).await))
+        let schema = self.schema.clone();
+
+        let graphql_post = async_graphql_warp::graphql(schema).and_then(
+            |(schema, request): (GraphQLSchema, async_graphql::Request)| async move {
+                let schema_res = schema.execute(request).await;
+                Ok::<_, Infallible>(Response::from(schema_res))
             },
         );
 
         let graphql_playground = warp::path::end().and(warp::get()).map(|| {
+            let playground_html =
+                playground_source(GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"));
+
             HttpResponse::builder()
                 .header("content-type", "text/html")
-                .body(playground_source(
-                    GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"),
-                ))
+                .body(playground_html)
         });
 
         let routes = graphql_subscription(self.schema.clone())
@@ -51,17 +53,27 @@ impl Server {
 
         let addr = SocketAddr::from(([0, 0, 0, 0], self.port));
 
+        println!("Starting the server on port {}", self.port);
+
         let _ = warp::serve(routes).run(addr).await;
 
         Ok(())
     }
 }
 
+#[derive(StructOpt)]
+struct Opts {
+    #[structopt(short = "p", long, default_value = "8080", env = "PORT")]
+    port: u16,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let opts = Opts::from_args();
+
     let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
 
-    let server = Server::new(8080, schema);
+    let server = Server::new(opts.port, schema);
 
     let _ = server.run().await?;
 
